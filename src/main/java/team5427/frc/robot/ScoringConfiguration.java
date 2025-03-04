9f15dad967545df1b704c50ba98d9483647916da
Saathvik k <saathvikkamma@gmail.com>
2025-03-03 21:02:33 -0600

    merrr

--- src/main/java/team5427/frc/robot/ScoringConfiguration.java
+++ src/main/java/team5427/frc/robot/ScoringConfiguration.java
@@ -1,4 +1,4 @@
-// package team5427.frc.robot;
+package team5427.frc.robot;
 
 import static edu.wpi.first.units.Units.Meter;
 
@@ -15,47 +15,47 @@ import team5427.lib.detection.tuples.Tuple5Plus;
 import team5427.lib.kinematics.inverse.ArmInverseKinematics;
 import team5427.lib.kinematics.inverse.ArmInverseKinematics.VariableLengthArm;
 
-// public class ScoringConfiguration {
-//   private Pose3d driveTrainTargetPose;
-//   private Translation3d endEffectorLocation;
-//   private Rotation2d cascadeAngle;
-//   private Distance cascadeLength;
-//   private Rotation2d endEffectorAngle;
+public class ScoringConfiguration {
+  private Pose3d driveTrainTargetPose;
+  private Translation3d endEffectorLocation;
+  private Rotation2d cascadeAngle;
+  private Distance cascadeLength;
+  private Rotation2d endEffectorAngle;
 
-//   private boolean isVariable = false;
+  private boolean isVariable = false;
 
-//   private ArmInverseKinematics generic_IK = new ArmInverseKinematics();
+  private ArmInverseKinematics generic_IK = new ArmInverseKinematics();
 
-//   private VariableLengthArm ik;
+  private VariableLengthArm ik;
 
-//   public ScoringConfiguration(){
-//     driveTrainTargetPose = new Pose3d();
-//     cascadeAngle = Rotation2d.kZero;
-//     cascadeLength = Meter.of(0.0);
-//     endEffectorAngle = Rotation2d.kZero;
-//     ik = generic_IK.new VariableLengthArm(ScoringConstants.kSegmentLengths, endEffectorAngle, endEffectorLocation);
-//   }
+  public ScoringConfiguration(){
+    driveTrainTargetPose = new Pose3d();
+    cascadeAngle = Rotation2d.kZero;
+    cascadeLength = Meter.of(0.0);
+    endEffectorAngle = Rotation2d.kZero;
+    ik = generic_IK.new VariableLengthArm(ScoringConstants.kSegmentLengths, endEffectorAngle, endEffectorLocation);
+  }
 
-//   public ScoringConfiguration(Pose3d driveTrainTargetPose, Rotation2d cascadeAngle, Distance casadeLength, Rotation2d endEffectorAngle){
-//     this.driveTrainTargetPose = driveTrainTargetPose;
-//     this.cascadeAngle = cascadeAngle;
-//     this.cascadeLength = casadeLength;
-//     this.endEffectorAngle = endEffectorAngle;
-//     ik = generic_IK.new VariableLengthArm(ScoringConstants.kSegmentLengths, endEffectorAngle, endEffectorLocation);
-//   }
+  public ScoringConfiguration(Pose3d driveTrainTargetPose, Rotation2d cascadeAngle, Distance casadeLength, Rotation2d endEffectorAngle){
+    this.driveTrainTargetPose = driveTrainTargetPose;
+    this.cascadeAngle = cascadeAngle;
+    this.cascadeLength = casadeLength;
+    this.endEffectorAngle = endEffectorAngle;
+    ik = generic_IK.new VariableLengthArm(ScoringConstants.kSegmentLengths, endEffectorAngle, endEffectorLocation);
+  }
 
-//   public ScoringConfiguration(Pose3d driveTrainTargetPose, Rotation2d cascadeAngle, Distance casadeLength, Rotation2d endEffectorAngle, boolean isVariable){
-//     this.driveTrainTargetPose = driveTrainTargetPose;
-//     this.cascadeAngle = cascadeAngle;
-//     this.cascadeLength = casadeLength;
-//     this.endEffectorAngle = endEffectorAngle;
-//     this.isVariable = isVariable;
-//     ik = generic_IK.new VariableLengthArm(ScoringConstants.kSegmentLengths, endEffectorAngle, endEffectorLocation);
-//   }
+  public ScoringConfiguration(Pose3d driveTrainTargetPose, Rotation2d cascadeAngle, Distance casadeLength, Rotation2d endEffectorAngle, boolean isVariable){
+    this.driveTrainTargetPose = driveTrainTargetPose;
+    this.cascadeAngle = cascadeAngle;
+    this.cascadeLength = casadeLength;
+    this.endEffectorAngle = endEffectorAngle;
+    this.isVariable = isVariable;
+    ik = generic_IK.new VariableLengthArm(ScoringConstants.kSegmentLengths, endEffectorAngle, endEffectorLocation);
+  }
 
   public Tuple4Plus<Pose3d, Rotation2d, Distance, Rotation2d> asTuple() {
     return new Tuple4Plus<Pose3d,Rotation2d,Distance,Rotation2d>(driveTrainTargetPose, cascadeAngle, cascadeLength, endEffectorAngle);
   }
 
 
-// }
+}
