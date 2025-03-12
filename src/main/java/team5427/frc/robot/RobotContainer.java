81d5fd4b87a0927e8c814e6dab6cb934396485fb
Saathvik k <saathvikkamma@gmail.com>
2025-03-12 12:42:43 -0500

    formating

--- src/main/java/team5427/frc/robot/RobotContainer.java
+++ src/main/java/team5427/frc/robot/RobotContainer.java
@@ -56,7 +56,8 @@ public class RobotContainer {
     AutoBuilder.configure(
         SwerveSubsystem.getInstance()::getPose, // Robot pose supplier
         SwerveSubsystem.getInstance()
-            ::resetAutonPose, // Method to reset odometry (will be called if your auto has a starting pose)
+            ::resetAutonPose, // Method to reset odometry (will be called if your auto has a
+        // starting pose)
         SwerveSubsystem.getInstance()
             ::getCurrentChassisSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
         // SwerveSubsystem.getInstance()::setChassisSpeeds,
